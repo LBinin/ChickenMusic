8a206130e9339b07217f046c11023432af8e6a1e
LBinin <linbin@lbinin.com>
2018-03-16 02:19:49 +0800

    添加进入离开播放器界面动画 Hook

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,21 +1,146 @@
 <template>
   <div class="player" v-show="playlist.length > 0">
-    <div class="normal-player" v-show="fullScreen">
-      播放器
-    </div>
-    <div class="mini-player" v-show="!fullScreen"></div>
+    <transition name="normal"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="afterLeave"
+    >
+      <div class="normal-player" v-show="fullScreen">
+        <div class="background">
+          <img width="100%" height="100%" :src="currSong.image">
+        </div>
+        <div class="top">
+          <div class="back" @click="back">
+            <i class="icon-back"></i>
+          </div>
+          <h1 class="title" v-html="currSong.name"></h1>
+          <h2 class="subtitle" v-html="currSong.singer"></h2>
+        </div>
+        <div class="middle">
+          <div class="middle-l">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class="cd">
+                <img class="image" :src="currSong.image">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bottom">
+          <div class="operators">
+            <div class="icon i-left">
+              <i class="icon-sequence"></i>
+            </div>
+            <div class="icon i-left">
+              <i class="icon-prev"></i>
+            </div>
+            <div class="icon i-center">
+              <i class="icon-play"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon-next"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon icon-not-favorite"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+    <transition name="mini">
+      <div class="mini-player" v-show="!fullScreen" @click="open">
+        <div class="icon">
+          <img width="40" height="40" :src="currSong.image">
+        </div>
+        <div class="text">
+          <h2 class="name" v-html="currSong.name"></h2>
+          <p class="desc" v-html="currSong.singer"></p>
+        </div>
+        <div class="control"></div>
+        <div class="control">
+          <i class="icon-playlist"></i>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
+import animations from 'create-keyframe-animation'
+import { prefixStyle } from 'common/lib/dom'
+
+const transform = prefixStyle('transform')
 
 export default {
   computed: {
-    ...mapGetters([
-      'fullScreen',
-      'playlist'
-    ])
+    ...mapGetters(['fullScreen', 'playlist', 'currSong'])
+  },
+  methods: {
+    back() {
+      this.setFullScreen(false) // 相当于 this.$store.commit('SET_FULL_SCREEN')
+    },
+    open() {
+      this.setFullScreen(true)
+    },
+    enter(el, done) {
+      const { x, y, scale } = this._getPosAndScale()
+
+      let animation = {
+        0: {
+          transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+        },
+        60: {
+          transform: `translate3d(0, 0, 0) scale(1.1)`
+        },
+        100: {
+          transform: `translate3d(0, 0, 0) scale(1)`
+        }
+      }
+
+      animations.registerAnimation({
+        name: 'move',
+        animation,
+        presets: {
+          duration: 500,
+          easing: 'ease-in-out'
+        }
+      })
+
+      animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+    },
+    afterEnter() {
+      animations.unregisterAnimation('move')
+      this.$refs.cdWrapper.style.animation = ''
+    },
+    leave(el, done) {
+      this.$refs.cdWrapper.style.transition = 'all 0.5s'
+      const { x, y, scale } = this._getPosAndScale()
+
+      this.$refs.cdWrapper.style[transform] = `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+      this.$refs.cdWrapper.addEventListener('transitionend', done)
+    },
+    afterLeave() {
+      this.$refs.cdWrapper.style.transition = ''
+      this.$refs.cdWrapper.style[transform] = ''
+    },
+    _getPosAndScale() {
+      const targetWidth = 40
+      const width = window.innerWidth * 0.8
+      const scale = targetWidth / width
+
+      // 以左下角为 (0, 0) 点
+      const miniX = 40
+      const normalPaddingTop = 80
+      const miniY = 30
+      const diffX = -(window.innerWidth / 2 - miniX)
+      const diffY = window.innerHeight - normalPaddingTop - width / 2 - miniY
+
+      return { x: diffX, y: diffY, scale }
+    },
+    ...mapMutations({
+      setFullScreen: 'SET_FULL_SCREEN'
+    })
   }
 }
 </script>
